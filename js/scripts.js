1b80cfbaba47143cc4449e6b09657a714e73bec4
Adeniyi Bella <adeniyibella@gmail.com>
2022-10-05 14:59:00 +0200

    Created an array of 3 objects of Pokemon types

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,35 @@
-alert('Hello world');
-
-let favoriteFood = 'Rice witn cream and Bacon'
-document.write(favoriteFood)
+// Creating an array of objects of Pokemon types
+let pokemonList = [
+    {
+      pokemonName: "Bulbasaur",
+      pokemonHeight: 0.7,
+      pokemonType: ['grass', 'poison'],
+      pokemonAbilities: ['Chlorophyll', 'Overgrow']
+    },
+    {
+      pokemonName: 'Charmeleon',
+      pokemonHeight: 1.1,
+      pokemonType: ['fire'],
+      pokemonAbilities: ['Blaze', 'Solar-power']
+    },
+    {
+      pokemonName: 'Fearow',
+      pokemonHeight: 1.2,
+      pokemonType: ['flying', 'normal'],
+      pokemonAbilities: ['Keen-eye', 'Sniper']
+    }
+  ];
+  
+//   // Writing to DOM name and Height of Pokemon
+//   pokemonList.forEach(pokemon => {
+//     let Name = pokemon.pokemonName
+//     let Height = pokemon.pokemonHeight
+//     // Checking if Pokemon is taller than 1m
+//     if (Height > 1.0) {
+//       document.write(`${Name} (height:${Height}m) - Wow That's Big!! <br><br>`)
+//     } else {
+//       document.write(`${Name} (height:${Height}m) <br><br>`)
+//     }
+//   });
+  
+  
