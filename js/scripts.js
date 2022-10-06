2337ebb5ef7dc609e75301065e6e10088952ae9f
Adeniyi Bella <adeniyibella@gmail.com>
2022-10-06 23:04:05 +0200

    Wrapped Pokemon list in an IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
 // Creating an array of objects of Pokemon types
-let pokemonList = [
+
+// console.log(typeof({}))
+let pokemonRepository = (function () {
+  let pokemonList = [
     {
       pokemonName: "Bulbasaur",
       pokemonHeight: 0.7,
       pokemonType: ['grass', 'poison'],
       pokemonAbilities: ['Chlorophyll', 'Overgrow'],
-     },
+    },
     {
       pokemonName: 'Charmeleon',
       pokemonHeight: 1.1,
@@ -18,19 +21,41 @@ let pokemonList = [
       pokemonType: ['flying', 'normal'],
       pokemonAbilities: ['Keen-eye', 'Sniper']
     }
-  ];
-  document.write('<p class= "heading">List of Pokemons with their height </p>')
-  // Writing to DOM name and Height of Pokemon
-  pokemonList.forEach(pokemon => {
-    let Name = pokemon.pokemonName
-    let Height = pokemon.pokemonHeight
-    // Checking if Pokemon is taller than 1m
-    if (Height > 1.0) {
-        // document.write(pokemon.imgPath)
-      document.write(`<p>${Name} (height:${Height}m) - Wow That's Big!! </p>`)
-    } else {
-      document.write(`<p>${Name} (height:${Height}m) </p>`)
+  ]
+
+  return {
+    add: function (pokemon) {
+      if (typeof (pokemon) === 'object') {
+        pokemonList.push(pokemon);
+      }
+      else { 'An object is expected' }
+    },
+
+    getAll: function () {
+      return pokemonList;
     }
-  });
-  
-  
+  }
+})();
+
+// Adding a new object to the Pokemon Array
+pokemonRepository.add({
+  pokemonName: 'Fearow',
+  pokemonHeight: 0.9,
+  pokemonType: ['flying', 'normal'],
+  pokemonAbilities: ['Keen-eye', 'Sniper']
+})
+
+document.write('<p class= "heading">List of Pokemons with their height </p>')
+
+// Writing to DOM name and Height of Pokemon
+pokemonRepository.getAll().forEach(pokemon => {
+  let Name = pokemon.pokemonName
+  let Height = pokemon.pokemonHeight
+  // Checking if Pokemon is taller than 1m
+  if (Height > 1.0) {
+    // document.write(pokemon.imgPath)
+    document.write(`<p>${Name} (height:${Height}m) - Wow That's Big!! </p>`)
+  } else {
+    document.write(`<p>${Name} (height:${Height}m) </p>`)
+  }
+});
